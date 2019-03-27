4baa916cc36c6900f0e531a6b9d257bc66118cb5
Dayanand <javaexpertdayanand@gmail.com>
2019-03-28 00:52:59 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data.service';
+
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'datatable';
+
+  userList = [];
+ constructor(private data : DataService){}
+
+ ngOnInit(){
+
+  // this.userList = this.data.User;
+
+ }
+
+ 
 }
